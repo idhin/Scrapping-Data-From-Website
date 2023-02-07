78a2f203249ed29d817c7f7fb8f5424b9e5703ba
idhin <kultusanmu@gmail.com>
2023-02-08 00:35:03 +0700

    add Function

--- scrap.py
+++ scrap.py
@@ -12,28 +12,38 @@ url_baru = []
 soup = BeautifulSoup(r.text)
 # print(soup.title.string)
 
-for link in soup.find_all('a'):
-    url = link.get('href')
-    if url and '/currencies/' in url and '/climate/united-states/us' not in url:
-        url_baru.append(url)
-# print(state_links)
 
-f = open("test.txt", "a")
+def prosesAmbilData():
+    for link in soup.find_all('a'):
+        url = link.get('href')
+        if url and '/currencies/' in url and '/climate/united-states/us' not in url:
+            url_baru.append(url)
+    # print(state_links)
 
-for i in url_baru:
-    if i.find("bitcoin") != -1:
-        pass
-    elif i.find("axie-infinity") != -1:
-        pass
-    elif i.find("axie-infinity") != -1:
-        pass
-    elif i.find("smooth-love-potion") != -1:
-        pass
-    elif i.find("ether") != -1:
-        pass
-    elif i.find("soup") != -1:
-        pass
-    else:
-        print(i.replace("/currencies/",
-              "https://coinmarketcap.com/currencies/"), file=f)
-f.close()
+    f = open("test.txt", "a")
+
+    for i in url_baru:
+        if i.find("bitcoin") != -1:
+            pass
+        elif i.find("axie-infinity") != -1:
+            pass
+        elif i.find("axie-infinity") != -1:
+            pass
+        elif i.find("smooth-love-potion") != -1:
+            pass
+        elif i.find("ether") != -1:
+            pass
+        elif i.find("soup") != -1:
+            pass
+        else:
+            print(i.replace("/currencies/",
+                            "https://coinmarketcap.com/currencies/"), file=f)
+    f.close()
+
+
+def main():
+    prosesAmbilData()
+
+
+if __name__ == "__main__":
+    main()
