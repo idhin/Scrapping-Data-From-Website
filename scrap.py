3b7d788d15c4c781f79f9dd6254dfcd849ce9edd
idhin <kultusanmu@gmail.com>
2023-02-08 00:01:03 +0700

    push to repo

---
+++ scrap.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get('https://coinmarketcap.com/new/')
+base_url = 'https://coinmarketcap.com'
+url_baru = []
+
+soup = BeautifulSoup(r.text)
+# print(soup.title.string)
+
+for link in soup.find_all('a'):
+    url = link.get('href')
+    if url and '/currencies/' in url and '/climate/united-states/us' not in url:
+        url_baru.append(url)
+# print(state_links)
+
+
+for i in url_baru:
+    print(i)
